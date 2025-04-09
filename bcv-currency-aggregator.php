dc4df50cabfbeba370a7f386ed67522218b4e3f8
amst1909 <ansi79@gmail.com>
2025-04-09 19:52:59 -0400

    corriendo error en la tasa de cambio

--- bcv-currency-aggregator.php
+++ bcv-currency-aggregator.php
@@ -10,37 +10,46 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// 1. Anunciar el agregador personalizado
-add_filter('woocs_announce_aggregator', 'bcv_pydolarve_announce');
-function bcv_pydolarve_announce($aggregators) {
-    $aggregators['bcvpydolar'] = 'BCV Venezuela (PyDolarVE)';
+// Anunciar nuestro nuevo agregador
+add_filter('woocs_announce_aggregator', function($aggregators){
+    $aggregators['pydolarve_bcv'] = 'PyDolarVE BCV';
     return $aggregators;
-}
+});
 
-// 2. Registrar el procesador del agregador
-add_filter('woocs_add_aggregator_processor', 'bcv_pydolarve_processor', 10, 2);
+// Procesamiento de tasas
+add_filter('woocs_add_aggregator_processor', function($aggregator_key, $currency_name){
+    global $WOOCS;
+    $request = 0;
 
-function bcv_pydolarve_processor($rate, $currency_name) {
-    if ($currency_name !== 'bcvpydolar') {
-        return $rate;
+    // Verificamos si es el agregador personalizado
+    if ($aggregator_key === 'pydolarve_bcv') {
+        // URL de la API
+        $query_url = 'https://pydolarve.org/api/v1/dollar?monitor=bcv';
+        
+        // Obtenemos el contenido
+        if (function_exists('curl_init')) {
+            $res = $WOOCS->file_get_contents_curl($query_url);
+        } else {
+            $res = @file_get_contents($query_url);
+        }
+
+        // Convertimos a objeto PHP
+        $data = json_decode($res, true);
+
+        // Verificamos y asignamos la tasa
+        if (isset($data['price'])) {
+            // Supongamos que $WOOCS->default_currency es USD y convertimos a VES
+            if ($WOOCS->default_currency === 'USD' && $currency_name === 'VES') {
+                $request = floatval($data['price']);
+            } else {
+                
+                // Por defecto, devolvemos la tasa en cero si no corresponde a VES
+                $request = 0;
+            }
+        } else {
+            $request = sprintf("No hay datos disponibles para %s", $currency_name);
+        }
     }
 
-    $response = wp_remote_get('https://pydolarve.org/api/v1/dollar?monitor=bcv', array(
-        'timeout' => 15,
-        'headers' => array(
-            'User-Agent' => 'Mozilla/5.0 (compatible; WordPress BCV Aggregator)'
-        )
-    ));
-
-    if (is_wp_error($response)) {
-        return $rate; // fallback
-    }
-
-    $data = json_decode(wp_remote_retrieve_body($response), true);
-
-    if (!isset($data['price'])) {
-        return $rate;
-    }
-    
-    return floatval($data['price']);
-}
+    return $request;
+}, 10, 2);
