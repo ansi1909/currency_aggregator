dde1cb846256cd3f59f365447810626f3e181360
amst1909 <ansi79@gmail.com>
2025-04-23 10:01:45 -0400

    se ajusto para que el cambio siempre mantenga el precio del día hasta las 11:59 am y de igual manera para que el precio del cambio del dolar se matenga desde el viernes hasta el domingo a las 11:59pm

--- bcv-currency-aggregator.php
+++ bcv-currency-aggregator.php
@@ -23,33 +23,81 @@ add_filter('woocs_add_aggregator_processor', function($aggregator_key, $currency
 
     // Verificamos si es el agregador personalizado
     if ($aggregator_key === 'pydolarve_bcv') {
-        // URL de la API
-        $query_url = 'https://pydolarve.org/api/v1/dollar?monitor=bcv';
         
-        // Obtenemos el contenido
-        if (function_exists('curl_init')) {
-            $res = $WOOCS->file_get_contents_curl($query_url);
-        } else {
-            $res = @file_get_contents($query_url);
-        }
+        $precio_dolar = obtener_dolar_bcv_estable();
 
-        // Convertimos a objeto PHP
-        $data = json_decode($res, true);
-
-        // Verificamos y asignamos la tasa
-        if (isset($data['price'])) {
-            // Supongamos que $WOOCS->default_currency es USD y convertimos a VES
-            if ($WOOCS->default_currency === 'USD' && $currency_name === 'VES') {
-                $request = floatval($data['price']);
-            } else {
-                
-                // Por defecto, devolvemos la tasa en cero si no corresponde a VES
-                $request = 0;
+        if(isset($precio_dolar)){
+            if($WOOCS->default_currency === 'USD' && $currency_name === 'VES'){
+                $request = $precio_dolar;
             }
-        } else {
-            $request = sprintf("No hay datos disponibles para %s", $currency_name);
         }
+
     }
 
     return $request;
 }, 10, 2);
+
+function obtener_dolar_bcv_estable(){
+    //Nombre del transient
+    $transient_key = 'dolar_bcv_fijo';
+    
+    //Revisa si ya está guardado el valor
+    $valor_guardado = get_transient($transient_key);
+    if($valor_guardado != false){
+        return $valor_guardado;
+    }
+
+    //Fecha y hora actual
+    $fecha_actual = new DateTime('now', DateTimeZone('America/Caracas'));
+    $hora_actual = (int) $fecha_actual->format('H');
+    $dia_semana = (int) $fecha_actual->format('N'); // Lunes = 1, Domingo = 7
+
+    // Si es viernes antes de las 5pm o un día de semana antes de las 5p
+    if($dia_semana <= 5 && $hora_actual < 17){
+        //Consumir API
+        $response = wp_remote_get('https://pydolarve.org/api/v1/dollar?monitor=bcv');
+
+        if(is_wp_error($response)){
+            return "Error al conectar con la API";
+        }
+
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+
+        if(!isset($data['price'])){
+            return 'Respuesta invalida de la API';
+        }
+
+        $precio = floatval($data['price']);
+
+        //Guardar por 24 horas, ya que se usará todo el fin de semana si es viernes
+        $tiempo_expriracion = ($dia_semana == 5) ? DAY_IN_SECONDS * 3 : HOUR_IN_SECONDS * (17 - $hora_actual);
+        set_transient($transient_key, $precio, $tiempo_expriracion);
+
+        return $precio;
+    }
+    // Si es después de las 5pm, sábado o domingo, devolver el valor fijo anterior
+    // Si aún no hay valor fijo (por error o primera ejecución), forzar uso de la API
+    if($valor_guardado == false){
+
+        $response = wp_remote_get('https://pydolarve.org/api/v1/dollar?monitor=bcv');
+
+        if(is_wp_error($response)){
+            return "Error al conectar con la API";
+        }
+
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+            
+        if(!isset($data['price'])){
+            return 'Respuesta invalida de la API';
+        }
+            
+        $precio = floatval($data['price']);
+        set_transient($transient_key, $precio, HOUR_IN_SECONDS * 12); //Evita quedar sin valor
+        return $precio;
+
+    }
+
+    return $valor_guardado;
+    
+}
+
